a343097d386d3a6dd949a8b157f016d9d1647743
Stefano Rasi <rasi.stefano@gmail.com>
2023-07-12 10:53:22 +0200

    Add keyboard input

--- main.rb
+++ main.rb
@@ -12,12 +12,42 @@ document = $$.document
 
 sequence = document.querySelector('#sequence').value
 
+keys = Set.new()
+
+document.addEventListener('keyup') do |event|
+    keys.delete(Native(event).key)
+end
+
+document.addEventListener('keydown') do |event|
+    keys.add(Native(event).key)
+end
+
 Game.init()
 
 width = Game.width
 height = Game.height
 
-Game.player = Player.new(width * 0.5, height * 0.8)
+player = Player.new(width * 0.5, height * 0.8)
+
+player.update do
+    x = 0
+    y = 0
+
+    keys.each do |key|
+        if key == 'ArrowUp' then y = -1 end
+        if key == 'ArrowDown' then y = 1 end
+        if key == 'ArrowLeft' then x = -1 end
+        if key == 'ArrowRight' then x = 1 end
+    end
+
+    if x == 0 && y == 0
+        player.direction = nil
+    else
+        player.direction = Math.atan2(y, x)
+    end
+end
+
+Game.player = player
 
 window.fetch(sequence).then do |response|
     Native(response).text().then do |text|
